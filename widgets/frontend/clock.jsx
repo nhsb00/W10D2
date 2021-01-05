14ea97cd6ac286877566679d9c365b4e232578b5
gabriel <gabeh.reiter@gmail.com>
2021-01-05 16:15:02 -0500

    css added

--- widgets/frontend/clock.jsx
+++ widgets/frontend/clock.jsx
@@ -3,12 +3,33 @@ import React from 'react';
 class Clock extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {time:new Date()};
+        this.tick = this.tick.bind(this);
+        
+    }
 
+    componentDidMount(){
+        this.interval = setInterval(this.tick, 1000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.interval)
+    }
+
+    tick(){
+        this.setState({ time: new Date() })
     }
 
     render () {
-        return (<h1>Clock</h1>
-            )
+        let hours = this.state.time.getHours()
+        let minutes = this.state.time.getMinutes()
+        let seconds = this.state.time.getSeconds()
+        return (
+            <div>
+                <h1>Clock</h1>
+                <p> {hours}:{minutes}:{seconds} </p>
+            </div>
+        )
     }
 }
 
